ab49ef4b3bff4ea584da162c6d5f3c6ab94f4b00
Narendra Shetty <mailnarendra15@gmail.com>
2016-06-19 18:32:46 +0530

    added tags container

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,19 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router';
 
 const TopBar = React.createClass({
 
+  renderTagList() {
+    let tags = this.props.Tags.get('tags').toArray().sort((a, b) => {
+      return b.get('value').size - a.get('value').size;
+    });
+    return (
+      <div className="tags__container">
+        <div className="tags">
+          {tags.map((tag) => {
+            return (
+              <Link to={{ pathname: '/', query: { tag: tag.get('key') } }} tagName="div" className="tag">
+                <span className="tag__name">{tag.get('key')}</span>
+                <span className="tag__count">{tag.get('value').size}</span>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    );
+  },
+
   render() {
     return (
-      <div className="topbar">
-        <div className="topbar__container">
-          <div className="topbar__title">
-            Flickr Clone
+      <div>
+        <div className="topbar">
+          <div className="topbar__container">
+            <div className="topbar__title">
+              Flickr Clone
+            </div>
           </div>
         </div>
+        {this.renderTagList()}
       </div>
     )
   }
 
 });
 
-export default TopBar;
+function mapStateToProps(state) {
+  return {
+    'Tags': state.Tags
+  };
+}
+
+export default connect(mapStateToProps)(TopBar);
